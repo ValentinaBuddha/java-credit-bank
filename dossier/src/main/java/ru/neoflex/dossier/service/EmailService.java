380ccdfa4326a4c69d0a932bbab7f799c05c73ab
Valentina Vakhlamova <valentina.buddha@yandex.ru>
2024-07-16 09:07:18 +0300

    dossier: refactor services

--- dossier/src/main/java/ru/neoflex/dossier/service/EmailService.java
+++ dossier/src/main/java/ru/neoflex/dossier/service/EmailService.java
@@ -1,8 +1,60 @@
 package ru.neoflex.dossier.service;
 
-public interface EmailService {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
-    void sendSimpleMessage(String to, String subject, String text);
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
-    void sendMessageWithAttachment(String to, String subject, String text);
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            emailSender.send(message);
+        } catch (Exception exception) {
+            log.error("Sending email exception: " + exception.getMessage());
+        }
+    }
+
+    public void sendMessageWithAttachment(String to, String subject, String text) {
+        try {
+            MimeMessage message = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+
+            String userHome = System.getProperty("user.home");
+            Path filePath = Paths.get(userHome, "IdeaProjects", "java-credit-bank", "dossier", "loan_documents.txt");
+            File file = new File(filePath.toString());
+            FileSystemResource documents = new FileSystemResource(file);
+            helper.addAttachment(Objects.requireNonNull(documents.getFilename()), documents);
+
+            emailSender.send(message);
+        } catch (Exception exception) {
+            log.error("Sending email exception: " + exception.getMessage());
+        }
+    }
 }
--- dossier/src/main/java/ru/neoflex/dossier/service/FileCreator.java
+++ dossier/src/main/java/ru/neoflex/dossier/service/FileCreator.java
@@ -1,8 +1,63 @@
 package ru.neoflex.dossier.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import ru.neoflex.dossier.dto.CreditDto;
+import ru.neoflex.dossier.dto.PaymentScheduleElementDto;
 
-public interface FileCreator {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 
-    void createTxtFile(CreditDto creditDto);
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class FileCreator {
+
+    public void createTxtFile(CreditDto creditDto) {
+
+        String userHome = System.getProperty("user.home");
+        Path filePath = Paths.get(userHome, "IdeaProjects", "java-credit-bank", "dossier", "loan_documents.txt");
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath.toString()))) {
+
+            writer.write("Credit Amount " + creditDto.getAmount() + "\n");
+            writer.write("Term " + creditDto.getTerm() + "\n");
+            writer.write("Monthly Payment " + creditDto.getMonthlyPayment() + "\n");
+            writer.write("Rate " + creditDto.getRate() + "\n");
+            writer.write("PSK " + creditDto.getPsk() + "\n");
+            if (Boolean.TRUE.equals(creditDto.getIsInsuranceEnabled())) {
+                writer.write("Insurance enabled\n");
+            } else {
+                writer.write("Insurance is not enabled\n");
+            }
+            if (Boolean.TRUE.equals(creditDto.getIsSalaryClient())) {
+                writer.write("Salary Client - Yes\n\n");
+            } else {
+                writer.write("Salary Client - No\n\n");
+            }
+
+            writer.write("Number,Date,Total Payment,Interest Payment,Debt Payment,Remaining Debt\n");
+            if (creditDto.getPaymentSchedule() != null) {
+                for (PaymentScheduleElementDto payment : creditDto.getPaymentSchedule()) {
+                    writer.write(payment.getNumber() + ",");
+                    writer.write(payment.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + ",");
+                    writer.write(payment.getTotalPayment() + ",");
+                    writer.write(payment.getInterestPayment() + ",");
+                    writer.write(payment.getDebtPayment() + ",");
+                    writer.write(payment.getRemainingDebt() + "\n");
+                }
+            }
+
+            log.info("File created successfully.");
+
+        } catch (IOException e) {
+            log.error("An error occurred while creating the file.");
+            e.printStackTrace();
+        }
+    }
 }
